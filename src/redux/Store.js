73909c1bd95c7f092e4f0aaff2473b187ed5d981
unknown <negi60239@gmail.com>
2024-09-25 20:09:18 +0530

    second commit

--- src/redux/Store.js
+++ src/redux/Store.js
@@ -1,5 +1,5 @@
 import { configureStore } from "@reduxjs/toolkit";
-import CartSlice from "./slice/CartSlice"; // Fixed the casing to be consistent
+import CartSlice from "./slice/Cartslice"; // Fixed the casing to be consistent
 import CategorySlice from "./slice/CategorySlice";
 import SearchSlice from "./slice/SearchSlice";
 import ThemeSlice from  "./slice/ModeSlice"
